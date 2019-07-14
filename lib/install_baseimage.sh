0bbbeb1d3c4dbf841188a732ab4136c4f0ca148a
George Theofilis <theofilis.g@gmail.com>
2019-07-14 17:47:18 +0300

    Use devuan:ascii as a base image

--- lib/install_baseimage.sh
+++ lib/install_baseimage.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #This is for base tag of image syneblock/quorum-master:base
+echo "deb http://ppa.launchpad.net/ethereum/ethereum/ubuntu bionic main" >> /etc/apt/sources.list.d/ethereum-bioinc.list
+echo "deb-src http://ppa.launchpad.net/ethereum/ethereum/ubuntu bionic main" >> /etc/apt/sources.list.d/ethereum-bioinc.list
+apt-get install dirmngr --install-recommends
+apt-key adv --keyserver keyserver.ubuntu.com  --recv-keys 2A518C819BE37D2C2031944D1C52189C923F6CA9
 apt-get update
+
 apt-get install -y libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
 apt-get install -y build-essential
-apt-get install software-properties-common python-software-properties -y
 apt-get install -y wget
 apt-get install -y curl
 apt-get install -y jq
 apt-get install -y psmisc
 apt-get install -y iputils-ping
-add-apt-repository ppa:ethereum/ethereum -y
-apt-get update
 apt-get install solc -y
 apt-get install bsdmainutils
 apt-get install -y openjdk-8-jdk
